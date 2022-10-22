519e339c8cc24dc40d13fdba64664f52d4738e1c
Akane277353 <39272953+Akane277353@users.noreply.github.com>
2022-10-22 19:06:04 +0200

    correction

--- afterreboot.sh
+++ afterreboot.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-cp -r ./config/* "$HOME"/.config
+cd i3_setup
+cp -r .config/* "$HOME"/.config
 sudo cp -r ./scripts/* /usr/local/bin
 sudo cp -r ./fonts/* /usr/share/fonts
 sudo cp -r ./tokyonight_gtk /usr/share/themes
-
-
-sudo cp ./keyitdev.zsh-theme /usr/share/oh-my-zsh/custom/themes
-cp ./.zshrc "$HOME"
